4de17627bb10bf3ee2de6150115b99a1ac808930
Nicole Tsingas <nicolecass27@gmail.com>
2023-05-26 10:16:54 -0300

    Alterado config do firebase pela Lari

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { app } from './firebase.config';
+import { app, db, collection, addDoc  } from './firebase.config';
 
 const auth = getAuth(app);
 
@@ -7,8 +7,19 @@ const registerUser = async (name, username, email, password) => {
   try {
     const userRegister = await createUserWithEmailAndPassword(auth, email, password);
     const user = userRegister.user;
+    const userData = {
+
+      name: name,
+      username: username,
+      email: email,
+
+    };
+
+    await addDoc(collection(db, 'users'), userData);
+
+    console.log('Usuário cadastrado com sucesso');
   } catch (error) {
-    console.log('Erro ao registrar usuário:', error.message);
+    console.log('Erro ao cadastrar usuário:', error.message);
   }
 };
 
