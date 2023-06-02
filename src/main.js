1365f41d62e603207ea4cb470a80c9b9d87478dc
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-06-02 09:42:58 -0300

    test

--- src/main.js
+++ src/main.js
@@ -1,35 +1,33 @@
 // Este es el punto de entrada de tu aplicacion - só manipulação de DOM - elements HTML
 
-<<<<<<< HEAD
-import { myFunction } from './lib/index.js';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC8zikyXf7G5y9OpRG2rppQF0ozv6LESCs",
-  authDomain: "social-network-af062.firebaseapp.com",
-  projectId: "social-network-af062",
-  storageBucket: "social-network-af062.appspot.com",
-  messagingSenderId: "887520259995",
-  appId: "1:887520259995:web:a5035a6804239ee20d3b3e"
+  apiKey: "AIzaSyBY3UkDzyQQt6bkMRjawouhmYdhqfv_Tag",
+  authDomain: "exemplo-89631.firebaseapp.com",
+  projectId: "exemplo-89631",
+  storageBucket: "exemplo-89631.appspot.com",
+  messagingSenderId: "489576568604",
+  appId: "1:489576568604:web:bd304747c79b7e2f498cd9",
+  measurementId: "G-JXS5G494MK"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-const email = "dunes@gmail.com"
+const email = "abacatee@frutas.com"
 const password = "123456"
 
 createUserWithEmailAndPassword(auth, email, password)
-
-=======
-import { myFunction, CriarUsuario } from './lib/index.js';
->>>>>>> 06fb8a4a82f03ce401d96d7faaac84667d4ebde1
-
-
 myFunction();
+
+
