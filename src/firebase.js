0ffb45bf8ce6ea0a3fc761291a7992e5d2292036
Fernanda Araújo de Souza <asouza.fernanda@gmail.com>
2023-06-02 12:29:27 -0300

    Configuração do Firebase

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIlJ7_NYHgBnZ3hTCABU5h51VR5oMz3bc",
+  authDomain: "rede-social-9ce25.firebaseapp.com",
+  projectId: "rede-social-9ce25",
+  storageBucket: "rede-social-9ce25.appspot.com",
+  messagingSenderId: "528688631413",
+  appId: "1:528688631413:web:b29bcb68a7c440f3e9f23c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const email = "abacate@frutas.com"
+const password = "123456"
+
+console.log (email)
+createUserWithEmailAndPassword(auth, email, password)
+const provider = new GoogleAuthProvider();
+
+
