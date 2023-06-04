5bb971c9b85dd8d695635da7f0822bfa0ef6f56a
alinepamplona <alinebpamplona@gmail.com>
2023-06-04 12:08:47 -0300

    Implementação do login com a conta Google

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import { myFunction } from './lib/index.js';
 myFunction();
 import {auth, app, db } from './firebaseConfig.js';
 
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "firebase/auth";
 
 const buttonEnter = document.getElementById('button-enter')
+const buttonLoginGoogle = document.getElementById('button-login-google')
 const inputEmail = document.getElementById('input-email')
 const inputPassword = document.getElementById('input-password')
 
@@ -17,7 +22,7 @@ buttonEnter.addEventListener("click", (event) => {
 
   createUserWithEmailAndPassword(auth, email, password)
    .then((userCredential) => {
-    // Signed in
+    // Signed
     const user = userCredential.user;
     // ...
     })
@@ -26,5 +31,23 @@ buttonEnter.addEventListener("click", (event) => {
     const errorMessage = error.message;
     // ..
     });
+
+})
+
+const provider = new GoogleAuthProvider();
+
+buttonLoginGoogle.addEventListener("click", (event) => {
+  signInWithPopup(auth, provider)
+
+  .then((result) => {
+    // O login com o Google foi bem-sucedido, você pode acessar as informações do usuário através de result.user
+    const user = result.user;
+  })
+  .catch((error) => {
+    // Ocorreu um erro durante o login com o Google
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
 
 })
