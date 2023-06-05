4808a5013ceaf73b5a8265422b3b965584a86a17
rbcribeiro <rbcribeiro@gmail.com>
2023-06-05 12:09:30 -0300

    Implementação da função criar usuário

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,31 +1,31 @@
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    GoogleAuthProvider,
-     } from 'firebase/auth';
-  import { app } from './app.js';
-  
-  export const getAppAuth = () => getAuth(app);
-  
-  export const getUserId = () => {
-    const auth = getAppAuth();
-    return auth.currentUser.uid;
-  };
-  
-  export const createUserWithEmail = (name, email, password) => {
-    const auth = getAppAuth();
-    return createUserWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        updateProfile(auth.currentUser, {
-          displayName: name,
-        });
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  updateProfile
+} from 'firebase/auth';
+import { app } from './app.js';
+
+export const getAppAuth = () => getAuth(app);
+
+export const getUserId = () => {
+  const auth = getAppAuth();
+  return auth.currentUser.uid;
+};
+
+export const createUserWithEmail = (name, email, password) => {
+  const auth = getAppAuth();
+  return createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      updateProfile(auth.currentUser, {
+        displayName: `${name}`,
       });
-  };
-  
-  
-  export const loginGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    const auth = getAppAuth();
-    return signInWithPopup(auth, provider);
-  };
-  
+    });
+};
+
+export const loginGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAppAuth();
+  return signInWithPopup(auth, provider);
+};
