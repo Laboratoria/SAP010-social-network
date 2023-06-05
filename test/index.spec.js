3a4501911ebe6bc0714dd39739f0d74f2d00440d
Larissa Paula Gonçalves <larissadepaulagoncalves@gmail.com>
2023-06-05 18:02:13 -0300

    teste para saber se o usuario esta logado feito com sucesso (por enquant)

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,15 @@
 import { isUserLoggedIn } from '../src/firebase/firebase.js';
 
-// mockar essa bagaça
-
-jest.mock('firebase/auth', () => ({
-  getAuth: () => ({
-    currentUser: {
-      displayName: 'Spock',
-      email: 'spoky@gmail.com',
-      uid: '3141592',
-    },
-  }),
-}));
+jest.mock('../src/firebase/firebase.js', () => {
+  const currentUser = {
+    displayName: 'Spock',
+    email: 'spoky@gmail.com',
+    uid: '3141592',
+  };
+  return {
+    isUserLoggedIn: async () => currentUser !== null,
+  };
+});
 
 describe('isUserLoggedIn', () => {
   it('deve retornar true quando o usuário estiver logado', async () => {
@@ -19,9 +18,7 @@ describe('isUserLoggedIn', () => {
   });
 
   it('deve retornar false quando o usuário não estiver logado', async () => {
-    jest.requireMock('firebase/auth').getAuth = () => ({
-      currentUser: null,
-    });
+    jest.requireMock('../src/firebase/firebase.js').isUserLoggedIn = async () => false;
     const result = await isUserLoggedIn();
     expect(result).toBe(false);
   });
