a67005c35dd1bc1952bc9e2e5b6a23bea76980e0
geanemr <geaneramos1995@gmail.com>
2023-06-05 11:24:18 -0300

    hashs entre paginas

--- src/main.js
+++ src/main.js
@@ -1,36 +1,32 @@
-// import { mostrarFormularioLogin } from './pages/login/login.js';
- //import { registrationFormView } from './pages/register/register.js';
-
-
-//window.onload = function() {
-  //const body = document.getElementsByTagName('body')[0];
-
-//   const titulo = document.createElement('h1');
-//   titulo.innerHTML = 'ExploraAí!';
-//   body.appendChild(titulo);
-
- // const loginBtn = createButton('ENTRAR', mostrarFormularioLogin);
- // body.appendChild(loginBtn);
-
- /*const registerBtn = createButton('CRIAR CONTA', mostrarFormularioRegistro);
- body.appendChild(registerBtn);
-}
-
-function createButton(text, onclick) {
- const button = document.createElement('button');
- button.innerHTML = text;
- button.onclick = onclick; // usar addEventListener nao onclick
-  return button;
- }*/
-
 import login from "./pages/login/login.js";
+import register from "./pages/register/register.js"
 
 
 const main = document.querySelector("#root");
 
+// const init = () => {
+    window.addEventListener ("hashchange", () =>{
+        main.innerHTML = "";
+        console.log(window.location.hash)
+        switch(window.location.hash){
+            case "#login":
+            main.appendChild(login());
+            break;
+            case "#register":
+            main.appendChild(register());
+            break;
+            default:
+            main.appendChild(login());
+        }
+    })
+// }
+
 
 window.addEventListener ("load", () =>{
     main.appendChild(login());
+    // init();
 })
+
+
 
 
