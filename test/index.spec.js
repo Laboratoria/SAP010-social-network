59e13315c98dc0c3319e65b45d0bdf09ff141e73
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-06-06 15:45:23 -0300

    ajuste teste de login com o google

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,5 @@
 // import { it } from 'node:test';
-// import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { expect, jest } from '@jest/globals';
+import { signInWithPopup } from 'firebase/auth';
 
 import {
   signInWithGoogle,
@@ -12,14 +11,10 @@ describe('signInWithGoogle', () => {
   it('deveria ser uma função', () => {
     expect(typeof signInWithGoogle).toBe('function');
   });
-  const loginGoogle = jest.fn().mockReturnValue({ id: '123456', email: 'meajudajesus@gmail.com' });
-  it('deveria logar com o google', () => signInWithGoogle(loginGoogle).then(() => {
-    expect(loginGoogle.mock.calls[0][1]).toBe('123456');
-  }));
 
-  /* it('Deveria logar o usuário com a conta do google', async () => {
+  it('Deveria logar o usuário com a conta do google', async () => {
     signInWithPopup.mockResolvedValueOnce();
     await signInWithGoogle();
     expect(signInWithPopup).toHaveBeenCalledTimes(1);
-  }); */
+  });
 });
