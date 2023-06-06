b4cafd8d0ae220bdd663f7fcbc23cf712a37a325
rbcribeiro <rbcribeiro@gmail.com>
2023-06-06 13:55:09 -0300

    style: Inclusão de input e botão compartilhar na timeline

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -1,6 +1,13 @@
 export default () => {
     const timeline = document.createElement('div');
-    timeline.innerHTML = '<h1>Bem-vindo à linha do tempo!</h1>';
+    timeline.innerHTML = /*html*/`
+
+    <h1>Bem-vindo à linha do tempo!</h1>
+    <div class="input-container">
+    <input type="text" class="input-mensagem" placeholder="COMPARTILHE UMA EXPERIÊNCIA ...">
+    <button class="botao-compartilhar">COMPARTILHAR</button>
+</div>
+    `;
   
     return timeline;
   };
