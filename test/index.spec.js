a15d5ae4eb90ea437ef16a46382935861c60dc52
unknown <mirandamartins05@gmail.com>
2023-06-06 14:06:44 -0300

    adicionado test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,37 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import { validatePassword, validateEmail } from '../src/components.js/register.js';
+import { loginCreate } from '../src/lib/index';
 
-describe('myFunction', () => {
+/* describe('myFunction', () => {
   it('debería ser una función', () => {
     expect(typeof myFunction).toBe('function');
+  });
+}); */
+
+describe('Validações de e-mail e senha', () => {
+  // validação de e-mail
+  describe('validateEmail', () => {
+    test('Deve retornar true para um e-mail válido', () => {
+      const validEmail = 'test@example.com';
+      expect(validateEmail(validEmail)).toBe(true);
+    });
+
+    test('Deve retornar false para um e-mail inválido', () => {
+      const invalidEmail = 'invalid_email';
+      expect(validateEmail(invalidEmail)).toBe(false);
+    });
+  });
+
+  // validação de senha
+  describe('validatePassword', () => {
+    test('Deve retornar true para uma senha válida', () => {
+      const validPassword = 'senha123';
+      expect(validatePassword(validPassword)).toBe(true);
+    });
+
+    test('Deve retornar false para uma senha inválida', () => {
+      const invalidPassword = 'senha';
+      expect(validatePassword(invalidPassword)).toBe(false);
+    });
   });
 });
