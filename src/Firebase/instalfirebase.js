e610c03724f93a5217b85f404390b290fa9e92de
Mislene Silva Moura <mislene.moura2000@gmail.com>
2023-06-09 11:07:36 -0300

    Criado pastas dos arquivos e adicionado  imagens para a criação do site

--- src/Firebase/instalfirebase.js
+++ src/Firebase/instalfirebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
 import { getAuth } from "firebase/auth";
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDSedT4wQAFd6nZqtK9xWVLPVSRAj9y9dE",
   authDomain: "rede-social-2c0e4.firebaseapp.com",
@@ -14,6 +11,6 @@ const firebaseConfig = {
   appId: "1:204673635885:web:c3aaf53fe7e32b3f024aa4"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
