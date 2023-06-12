39c8f8c3f5494ec0516c52ea9fc84e7a027bc39b
Josielen Rubin <josirubinc@hotmail.com>
2023-06-12 11:23:12 -0300

    Configurações iniciais do Firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+import { auth, provider } from './lib/configFirebase.js';
+import { createUserWithEmailAndPassword, GoogleAuthProvider } from 'firebase/auth';
+
+const email = 'teste@coffeestation.com';
+const password = '123456';
+
+createUserWithEmailAndPassword(auth, email, password);
