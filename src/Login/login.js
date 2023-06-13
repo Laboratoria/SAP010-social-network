8c5d04fa89a58e9f08ec8155318985dea289a10d
Roxane Principe <roxane.principe@gmail.com>
2023-06-13 16:47:33 -0300

    Adicionado algumas estilizações

--- src/Login/login.js
+++ src/Login/login.js
@@ -4,43 +4,45 @@ const container = document.createElement('div');
 
 const template = `
 <div class="backgroundTwo">
-<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
-</svg>
+  <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+      <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+  </svg>
 </div>
 <div class="imagens">
-<img class="imagemLogo" src="Img/ImagemDesktopmap.png" alt= "Imagem Ilustrativa de pessoas interagindo">
-<img class="imagemMap" src="Img/ImagemMap.png" alt= "Mapa de GPS Ilustrativo">
+  <img class="imagemLogo" src="Img/ImagemDesktopmap.png" alt= "Imagem Ilustrativa de pessoas interagindo">
+  <img class="imagemMap" src="Img/ImagemMap.png" alt= "Mapa de GPS Ilustrativo">
 </div>
 <section class="login-page">
-<section class="form">
-  <form class="login-form show" method="post" id="login">
-    <section class="titulos">
+  <section class="form">
+    <form class="login-form show" method="post" id="login">
+      <section class="titulos">
+        <h1 class="titulo">Food Review</h1>
+      </section>
+      <section>
+        <input id=txtEmail type="text" placeholder="Email" required />
+        <input id=txtPassword type="password" placeholder="Senha" required/>
+        <div class="buttons">
+          <button class="full-width" id=btnLogin type="submit" name="send2">Entrar</button>
+          <div class="social">
+            <img class="logo" src="Img/Google.png" alt= "Logo Google">
+            <img class="logo" src="Img/facebook.png" alt= "Logo Facebook">
+          </div>
+        </div>
+        <p class="message">Não possui uma conta? <a href="#" id="newAccount">Cadastrar</a></p>
+      </section>
+    </form>
+    <form class="register-form" method="POST" id="register">
       <h1 class="titulo">Food Review</h1>
-      <p class="subtitulo1">O melhor site de avaliações do Brasil</p>
-      <p class="subtitulo2">Acesse e saiba mais!!</p>
-    </section>
-    <section>
-      <input id=txtEmail type="text" placeholder="Email" required />
-      <input id=txtPassword type="password" placeholder="Senha" required/>
-      <button id=btnLogin type="submit" name="send2">Entrar</button>
-      <p class="subtitulo2">Ou</p>
-      <img class="logo" src="Img/Google.png" alt= "Logo Google">
-      <img class="logo" src="Img/facebook.png" alt= "Logo Facebook">
-      <p class="message">Não possui uma conta? <a href="#" id="newAccount">Cadastrar</a></p>
-    </section>
-  </form>
-  <form class="register-form" method="POST" id="register">
-    <h1 class="titulo">Food Review</h1>
-    <p class="subtitulo2">Preencha os campos abaixo:</p>
-    <input type="text" placeholder="Nome completo *" required/>
-    <input type="email" placeholder="Email *" required/>
-    <input type="password" placeholder="Senha *" required/>
-    <button type="submit">Cadastrar</button>
-    <button type="submit" id="signup">Voltar</button>
-    <p class="message">Já tem uma conta? Clique em voltar e acesse com seu email e senha.</a></p>
-  </form>
-</section>
+      <input type="text" placeholder="Nome completo *" required/>
+      <input type="email" placeholder="Email *" required/>
+      <input type="password" placeholder="Senha *" required/>
+      <div class="buttons"> 
+        <button type="submit">Cadastrar</button>
+        <button type="submit" id="signup">Voltar</button>
+      </div>
+      <p class="message">Já tem uma conta? Clique em voltar e acesse.</a></p>
+    </form>
+  </section>
 </section>
 `;
 
