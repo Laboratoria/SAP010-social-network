ff2f6202eb4293cff4ecda82cf8719d06afe0e30
Mislene Silva Moura <mislene.moura2000@gmail.com>
2023-06-14 14:31:16 -0300

    Adicionado fechamento das tag section

--- src/Register/register.js
+++ src/Register/register.js
@@ -4,19 +4,19 @@ import '../Login/login.css';
 export const registerUser = () => {
   const container = document.createElement('div');
   const template = `
-<form class="register-form" method="POST" id="register">
-    <h1 class="titulo">Food Review</h1>
-    <p class="subtitulo2">Preencha os campos abaixo:</p>
-    <input type="text" placeholder="Nome completo *" required/>
-    <input type="email" placeholder="Email *" required/>
-    <input type="password" placeholder="Senha *" required/>
-    <button type="submit">Cadastrar</button>
-    <button type="submit" id="signup">Voltar</button>
-    <p class="message">Já tem uma conta? Clique em voltar e acesse com seu email e senha.</a></p>
-  </form>
-</section>
-</section>
-`;
+    <section class="form">
+      <form class="register-form" method="POST" id="register">
+        <h1 class="titulo">Food Review</h1>
+        <p class="subtitulo2">Preencha os campos abaixo:</p>
+        <input type="text" placeholder="Nome completo *" required/>
+        <input type="email" placeholder="Email *" required/>
+        <input type="password" placeholder="Senha *" required/>
+        <button type="submit">Cadastrar</button>
+        <button type="submit" id="signup">Voltar</button>
+        <p class="message">Já tem uma conta? Clique em voltar e acesse com seu email e senha.</a></p>
+      </form>
+    </section>
+  `;
 
   container.innerHTML = template;
   return container;
