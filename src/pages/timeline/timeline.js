3e6295507da7b3fa2ffbf74ca596bde4045ae719
geanemr <geaneramos1995@gmail.com>
2023-06-19 18:44:37 -0300

    auth: inserção dos botões e ícones de like, edit e delete, e resolução dos erros de eslint

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -1,9 +1,5 @@
-import { async } from 'regenerator-runtime';
 import { getUserName } from '../../firebase/auth.js';
-import {
-createPost,
-accessPost
-} from '../../firebase/firestore.js';
+import { createPost, accessPost } from '../../firebase/firestore.js';
 
 export default () => {
   const timeline = document.createElement('div');
@@ -15,7 +11,7 @@ export default () => {
         <button class='shareBtn' id='sharePost' >COMPARTILHAR</button>
         <div id='postList'></div>
       </div>
-      <!-- <figure class='image-register'>
+      <!-- <figure class='image-timeline'>
       <img src='img/assets/imagetimeline.png' class='img-timeline' alt='timelineImage'>
       </figure>-->
     </div>
@@ -27,16 +23,23 @@ export default () => {
   const descriptionPost = timeline.querySelector('#postArea');
   const postList = timeline.querySelector('#postList');
 
-  const CreatePostElement = (name,description) => {
+  const CreatePostElement = (name, description) => {
     const postElement = document.createElement('div');
     postElement.innerHTML = `
     <div class="post-container">
     <p class='nameUser'>${name}</p>
     <p class='textPost'>${description}</p>
+    <figure class='image-icons'>
+    <nav>
+      <a class='btn-like' id='likePost'><img src='img/assets/likeicon.png' alt='like image' width='30px'></a>
+      <a class='btn-edit' id='editPost'><img src='img/assets/editicon.png' alt='edit image' width='30px'></a>
+      <a class='btn-delete' id='deletePost'><img src='img/assets/deleteicon.png' alt='delete image' width='30px'></a>
+    </nav> 
+    </figure>
     </div>
     `;
     return postElement;
-  }
+  };
 
   postBtn.addEventListener('click', () => {
     const description = descriptionPost.value;
@@ -46,7 +49,7 @@ export default () => {
     } else {
       createPost(description)
         .then(() => {
-          descriptionPost.value = ''; /*limpa o input*/
+          descriptionPost.value = ''; /* limpa o input */
           loadPosts();
           alert('Publicação efetuada com sucesso!');
         })
@@ -65,7 +68,6 @@ export default () => {
       postList.appendChild(postElement);
     });
   };
-  
   loadPosts();
   return timeline;
 };
