9508d1468097e5ed82747f07c2eba7392768f4a9
geanemr <geaneramos1995@gmail.com>
2023-06-19 10:28:29 -0300

    fix: retirada de comentarios e funcoes extras

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -39,31 +39,3 @@ export async function accessPost() {
   });
   return allPosts;
 }
-
-// export const createPost = (description) => {
-//   const auth = getAppAuth();
-//   const userPost = auth.currentUser;
-//   const postComment = document.getElementById('ExplorAi').value;
-//   const userName = userPost.displayName;
-//   const userEmail = userPost.email;
-
-//   addDoc(collection(db, 'posts'), {
-//     name: userName,
-//     author: auth.currentUser.uid,
-//     email: userEmail,
-//     postContent: postComment,
-//     date: new Date(),
-//     like: [],
-//   });
-// };
-// export async function getPost() {
-//   const allPosts = [];
-//   const postQuery = query(collection(db, 'posts'), orderBy('createdAt', 'desc'));
-//   const querySnapshot = await getDocs(postQuery);
-//   querySnapshot.forEach((post) => {
-//     const data = post.data();
-//     data.id = post.id;
-//     allPosts.push(data);
-//   });
-//   return allPosts;
-// }
