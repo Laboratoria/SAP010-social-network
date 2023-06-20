dec9c86994deff9c30e931d305b2ef7de64f5a4e
Aline <128093293+Alinedev85@users.noreply.github.com>
2023-06-19 22:20:10 -0300

    msg de erro login

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,16 +1,54 @@
-import './register.css';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import {  validateRegister } from '../../configFirebase/auth';
+import { createUserWithEmail } from '../../firebase/auth.js';
 
 export default () => {
-  const container = document.createElement('div');
+  function getInputValues() {
+    const name = document.getElementById('nameRegister').value;
+    const email = document.getElementById('emailRegister').value;
+    const password = document.getElementById('senhaUsuario').value;
+    const areaAtuacao = document.getElementById('atuaçao').value;
+
+    return {
+      name,
+      email,
+      password,
+      areaAtuacao,
+    };
+  }
+
+  function printErrorMessage(message) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = message;
+  }
+
+  async function registerUser() {
+    const { name, email, password } = getInputValues();
+    const validationErrors = validateRegister(name, email, password);
+
+    if (validationErrors.length > 0) {
+      printErrorMessage(validationErrors);
+      return;
+    }
+
+    try {
+      await createUserWithEmail(name, email, password);
+      window.location.hash = '#timeline';
+    } catch (error) {
+      console.error('Erro ao registrar o usuário:', error);
+      const printError = errorsFirebase(error.code);
+      printErrorMessage(printError || 'Erro ao registrar o usuário');
+    }
+  }
+
+  const registrationForm = document.createElement('div');
 
   const templateRegister = `
-  <div class="imagens">
-  <img class="logo-cs" src="./images/logo2.png">
-  <img class="imagem-fundo" src="./images/background-desktop.png">
-  </div>
+    <div class="imagens">
+      <img class="logo-cs" src="./images/logo2.png">
+      <img class="imagem-fundo" src="./images/background-desktop.png">
+    </div>
     <fieldset>
-      <h2> *CAMPOS OBRIGATÓRIOS  </h2>
+      <h2>*CAMPOS OBRIGATÓRIOS</h2>
       <div class="cadastro">
         <label for="name">Nome Completo*</label>
         <input type="text" id="nameRegister" placeholder="Digite seu Nome" required>
@@ -18,43 +56,31 @@ export default () => {
         <input type="email" id="emailRegister" placeholder="Digite seu melhor e-mail" required>
         <label for="senha">Crie sua Senha*</label>
         <input type="password" id="senhaUsuario" placeholder="Digite sua senha" required>
-      <select class="areaDeAtuacao" id="atuaçao" required>
-        <option value="" hidden>Selecione a sua área de atuação*</option>
-        <option value="produtor">Produtor de café</option>
-        <option value="empresario">Empresário do ramo de café</option>
-        <option value="comprador">Comprador ou negociante de café</option>
-        <option value="outros">Outros</option>
-      </select>
+        <select class="areaDeAtuacao" id="atuaçao" required>
+          <option value="" hidden>Selecione a sua área de atuação*</option>
+          <option value="produtor">Produtor de café</option>
+          <option value="empresario">Empresário do ramo de café</option>
+          <option value="comprador">Comprador ou negociante de café</option>
+          <option value="outros">Outros</option>
+        </select>
       </div>
       <div class="botoes">
-      <button type="button" id="btn-register">Registrar</button>
-      <button type="button" id="btn-home">Voltar</button>
+        <button type="button" id="btn-register">Registrar</button>
+        <button type="button" id="btn-home">Voltar</button>
       </div>
+      <p id="error-message" class="error-message"></p>
     </fieldset>
   `;
 
-  container.innerHTML = templateRegister;
+  registrationForm.innerHTML = templateRegister;
 
-  const nameInput = container.querySelector('#nameRegister');
-  const emailInput = container.querySelector('#emailRegister');
-  const senhaInput = container.querySelector('#senhaUsuario');
-  const registerButton = container.querySelector('#btn-register');
-  const homeButton = container.querySelector('#btn-home')
+  const registerButton = registrationForm.querySelector('#btn-register');
+  const homeButton = registrationForm.querySelector('#btn-home');
 
-  const registrarNovoUsuario = () => {
-    const name = nameInput.value;
-    const email = emailInput.value;
-    const senha = senhaInput.value;
-    const areaAtuacao = container.querySelector('#atuaçao').value;
-
-    console.log('Nome:', name);
-    console.log('E-Mail:', email);
-    console.log('Área de Atuação:', areaAtuacao);
-  };
-
-  registerButton.addEventListener('click', registrarNovoUsuario);
+  registerButton.addEventListener('click', registerUser);
   homeButton.addEventListener('click', () => {
     window.location.hash = '';
   });
-  return container;
+
+  return registrationForm;
 };
