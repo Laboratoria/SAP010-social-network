72192928daa9244ae15d0cc20e34bfa222f14e2b
Josielen Rubin <josirubinc@hotmail.com>
2023-06-20 14:40:27 -0300

    Correção de estilos da mensagem do erro e da página de registro

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -18,8 +18,8 @@ export default () => {
         <input type="email" id="emailRegister" placeholder="Digite seu melhor e-mail" required>
         <label for="senha">Crie sua Senha*</label>
         <input type="password" id="senhaUsuario" placeholder="Digite sua senha" required>
+        <p>Selecione a sua área de atuação*</p>
       <select class="areaDeAtuacao" id="atuaçao" required>
-        <option value="" hidden>Selecione a sua área de atuação*</option>
         <option value="produtor">Produtor de café</option>
         <option value="empresario">Empresário do ramo de café</option>
         <option value="comprador">Comprador ou negociante de café</option>
