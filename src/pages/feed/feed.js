34c39d91a5a49fc0c0af4c8413c322edbb94fbd7
Josielen Rubin <josirubinc@hotmail.com>
2023-06-27 12:54:23 -0300

    Alterações no CSS do FEED e criação da página CAFÉ COM RUM

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -6,57 +6,42 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-  <body>
-  <div class="feed">
-    <div class="box">
-      <img class="logoCsFeed" src="./images/logo5.png">
-      <h1  class="conecteh1">CONECTE - COLABORE - CULTIVE</h1>
+  <div class="container">
 
-    
+      <div class="logo"> 
+      <img src="./images/logo5.png">
+      </div>
 
-        <div class="usuario">
-       
-         <h2 class="fulana">Olá, fulaninho(a);</h2>
-         <h3 class=convite>Possui um convite? Acesse:</h3>
-
+        <section class="menu">
+         <h2 class="saudacao">Olá, <span id="nome-usuario">fulaninho(a)</span>!</h2>
+         <h3 class="convite">Possui um convite? Acesse:</h3>
          <ul>
-           <li><a href="about:blank">Café com Rum</a></li>
+           <li><a href="#cafeComRum">Café com Rum</a></li>
          </ul>
-          <span id="username"></span>
           <button id="logoutButton" class="logout">Deslogar</button>
-
-        </div>
+        </section>
         
-        <div class="container2">
-         <h2 class="acesseTbm"> Acesse também:</h2>
+        <section class="filtros">
+         <h2 class="saudacao">Acesse também:</h2>
          <ul>
            <li><a href="https://www.conab.gov.br/" target="_blank">Atualizações da Conab.gov.br</a></li>
            <li><a href="https://app.powerbi.com/view?r=eyJrIjoiNDdkNDM4ZjctYzk0OS00NWVjLWFlYjktZWQ4Njg3MDEyMTg0IiwidCI6ImU2ZDkwZGYzLWYxOGItNGJkZC04MDhjLWFhNmQwZjY4YjgwOSJ9" target="_blank">Busca por Armazenadores</a></li>
             <li><a href="https://portaldeinformacoes.conab.gov.br/produtos-360.html" target="_blank">Acompanhe o preço da saca</a></li>
           </ul>
-
-        </div>
+          <img class="fundo" src="./images/background-filtros.png">
+        </section>
      
-
         <div class="postagens">
+         <textarea class="inputMensagem" id="areaMensagem" placeholder="Compartilhe ideias e informações sobre café"></textarea>
+         <button class="btnPostagem" id="postagemID"><img src="./images/icons/enviar-publicacao.svg"></button>
+         </div>
 
-         <div class="inputPostagem">
-         <textarea class="inputMensagem" id="areaMensagem" placeholder="Compartilhe ideias e informações sobre café "></textarea>
-         <button class="btnPostagem" id="postagemID">COMPARTILHAR</button>
-         </div>
-         </div>
          <div class="postagem">
-          
         </div>
-       
-    </div>
-  </div>
-</body>
 
+      </div>
   `;
 
   container.innerHTML = template;
   return container;
 };
-
-
