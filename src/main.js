8a6914ddf72a40cb3c050854ae41c23ddcd9c57d
maristvs <mariannalves5zoo@gmail.com>
2023-06-28 15:23:16 -0300

    nada importante

--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
 // Este es el punto de entrada de tu aplicacion
+import createUserWithEmailAndPassword from "./lib/configFirebase";
 
-import { myFunction } from './lib/index.js';
 
-myFunction();
+// import { myFunction } from './lib/index.js';
+
+// myFunction();
+
+createUserWithEmailAndPassword();
+
+// import cadastro from "./pages/cadastro/cadastro.js";
+import login from "./pages/login/login.js";
+import home from "./pages/home/home.js";
+// import loading from "./pages/loading";
+
+// loading();
+
+const main = document.querySelector("#root");
+const init = () => {
+  window.addEventListener("hashchange", () => {
+    // console.log(window.location.hash)
+    main.innerHTML = " ";
+    switch (window.location.hash) {
+      case " ":
+        main.appendChild(home());
+        break;
+      case "#login":
+        main.appendChild(login());
+        break;
+      // case "#contacts":
+      //   main.appendChild(contact());
+      //   break;
+      default:
+        main.appendChild(home());
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  main.appendChild(home());
+  init();
+});
