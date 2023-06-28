4263f204268d1574ecc031599b12430621b83e3c
rbcribeiro <rbcribeiro@gmail.com>
2023-06-28 10:21:53 -0300

    style: ajustes timeline e footer

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -13,15 +13,15 @@ export default () => {
   const viewPost = `
   <div class="container">
     <div class='left-timeline'>
-      <img src='./img/assets/icon-photo.png' alt='Foto de perfil' class='profilePhoto'>
+      <img src='./assets/icon-photo.png' alt='Foto de perfil' class='profilePhoto'>
       <p class="postTitle">Olá ${getUserName()}, bem-vindo(a) de volta!</p>
       <figure class='icones'>
-        <button type="button" class='button-timeline' id='home-btn'><img src="./img/assets/icon-home.png" class="icon-timeline" alt="Icone home">
-        <button type="button" class='button-timeline' id='logout-btn'><img src='./img/assets/icon-sair.png' class="icon-timeline" alt='logout icon'>
+        <button type="button" class='button-timeline' id='home-btn'><img src="./assets/icon-home.png" class="icon-timeline" alt="Icone home">
+        <button type="button" class='button-timeline' id='logout-btn'><img src='./assets/icon-sair.png' class="icon-timeline" alt='logout icon'>
       </figure>
       <input type="file" id="profilePhotoInput" accept="image/*" style="display: none;">
     </div>
-    <img src="./img/assets/imagetimeline.png" class="img-timeline" alt="edit image" >
+    <img src="./assets/imagetimeline.png" class="img-timeline" alt="edit image" >
     <div class="right-timeline">
       <div class="input-container">
         <textarea class="input-message" id="postArea" placeholder="COMPARTILHE UMA EXPERIÊNCIA..."></textarea>
@@ -67,18 +67,18 @@ export default () => {
         </div>
         <p class='textPost'>${description}</p>
         <div class='image-icons'>
-        <span class='likePost' id='likes-counter-${postId}'>${whoLiked.length}</span>
-        <button type="button" class='icons' id='like-Post' data-post-id='${postId}'>
-        <a class='icons' id='likePost'><img src='./img/assets/likeicon.png' alt='like image' class='icons'></a>
-      </button>
-      ${authorId === getUserId() ? `<button type="button" data-post-id='${postId}' class='icons' id='editPost'>
-      <a class='icons'><img src='./img/assets/editicon.png' alt='edit image' class='icons'></a>
-    </button>
-    <button type="button" class='icons' id='btn-delete' data-post-id='${postId}'>
-    <img src='./img/assets/deleteicon.png' alt='delete image' class='icons'>
-    </button>` : ''}
-    </div>
-  </div>
+          <span class='likePost' id='likes-counter-${postId}'>${whoLiked.length}</span>
+          <button type="button" class='icons-post' id='like-Post' data-post-id='${postId}'>
+            <a class='icons-post' id='likePost'><img src='./assets/likeicon.png' alt='like image' class='icons-post'></a>
+          </button>
+          ${authorId === getUserId() ? `<button type="button" data-post-id='${postId}' class='icons-post' id='editPost'>
+            <a class='icons-post'><img src='./assets/editicon.png' alt='edit image' class='icons-post'></a>
+          </button>
+          <button type="button" class='icons-post' id='btn-delete' data-post-id='${postId}'>
+            <img src='./assets/deleteicon.png' alt='delete image' class='icons-post'>
+          </button>` : ''}
+        </div>
+      </div>
 `;
 
     if (profilePhotoUrl) {
