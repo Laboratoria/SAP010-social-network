5fd141c69aa4477746c59303aeeee3fc091f5045
Vitória Victor <vitoriavictor@Vitorias-MacBook-Pro.local>
2023-06-29 00:48:05 -0300

    links botoes paginas funcionando, mudando hash da url, recebendo dados dos inputs

--- src/pages/Cadastro/cadastro.js
+++ src/pages/Cadastro/cadastro.js
@@ -1,34 +1,66 @@
-//colocar import do configfirebase.js e separar 
-//import { } from "../../lib/authUser.js";
+//import { createUserWithEmail } from '../../configFirebase/auth';
 
 export default () => {
-
-  const cadastro = document.createElement('div');
-  const templateCadastro = `<form action="">
-  <fieldset>Cadastre-se
-      <div>
-          <label for="">Nome completo</label>
+  const cadastroContainer = document.createElement('div');
+  const templateCadastro = `
+    <form action="">
+      <fieldset>
+        <legend>Cadastre-se</legend>
+        <div>
+          <label for="nome-completo">Nome completo</label>
           <input type="text" class="nome-completo" id="nome-completo">
-</div>
-      <div>
-          <label for="">Nome do usuário</label>
+        </div>
+        <div>
+          <label for="usuario">Nome do usuário</label>
           <input type="text" class="usuario" id="usuario">
-      </div>
-      <div>
+        </div>
+        <div>
           <label for="email">Email</label>
           <input type="email" class="email" id="email">
-      </div>
-      <div>
-          <label for="">Senha</label>
-          <input type="number" class="senha" id="senha">
-      </div>
-    <button class="btn" id="btn-cad-voltar">Voltar</button>
-    <button class="btn" id="btn-cad-concluir">Concluir cadastro</button>
-    <button class="btn" id="btn-cad-login-google">Login com Google</button>
-  </fieldset>
-</form>`;
-  
-  cadastro.innerHTML = templateCadastro;
-  
-  return cadastro;
-  }
+        </div>
+        <div>
+          <label for="senha">Senha</label>
+          <input type="password" class="senha" id="senha">
+        </div>
+        <button class="btn" id="btn-cad-voltar">Voltar</button>
+        <button class="btn" id="btn-cad-concluir">Concluir cadastro</button>
+        <button class="btn" id="btn-cad-login-google">Login com Google</button>
+      </fieldset>
+    </form>`;
+
+  cadastroContainer.innerHTML = templateCadastro;
+
+  const nomeEntrada = cadastroContainer.querySelector('#nome-completo');
+  const nomeUsuarioEntrada = cadastroContainer.querySelector('#usuario');
+  const emailEntrada = cadastroContainer.querySelector('#email');
+  const senhaEntrada = cadastroContainer.querySelector('#senha');
+  const botaoConcluir = cadastroContainer.querySelector('#btn-cad-concluir');
+  const botaoVoltar = cadastroContainer.querySelector('#btn-cad-voltar');
+
+  const registerUser = () => {
+    const name = nomeEntrada.value;
+    const usuario = nomeUsuarioEntrada.value;
+    const email = emailEntrada.value;
+    const senha = senhaEntrada.value;
+
+    // Chamada para a função createUserWithEmail
+    // createUserWithEmail(name, usuario, email, senha)
+    //   .then(() => {
+    //     // Cadastro concluído com sucesso
+    //   })
+    //   .catch((error) => {
+    //     // Lidar com erros durante o cadastro
+    //   });
+
+    // Teste cadastro
+    console.log(`Nome: ${name} Usuário: ${usuario} Email: ${email} Senha: ${senha}`);
+  };
+
+  botaoConcluir.addEventListener('click', registerUser);
+
+  botaoVoltar.addEventListener('click', () => {
+    window.location.hash = '';
+  });
+
+  return cadastroContainer;
+};
