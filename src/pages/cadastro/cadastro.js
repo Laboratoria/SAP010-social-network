49ac345a90a879bee6a37d3ec8c155928816ec01
amandascam03 <amandascam03@gmail.com>
2023-07-07 15:45:14 -0300

    adicionando inputs e button da segunda pagina

--- src/pages/cadastro/cadastro.js
+++ src/pages/cadastro/cadastro.js
@@ -10,8 +10,11 @@ const template = `<picture id="picture-cadastro">
 <img src="imagens/fightback-logo.png" alt="logo do app">
 </picture>
 </header>
-<form id="formulario-cadastro">
-
+<form id="formulario-cadastro" autocomplete="off">
+<input type="text" id="input-name" name="name" placeholder="Nome">
+<input type="email" id="input-email" name="email" placeholder="Email">
+<input type="password" id="input-senha" name="senha" placeholder="Senha">
+<button id="btn-vermelho">Cadastrar</button>
 </form>`;
 
 container.innerHTML = template;
