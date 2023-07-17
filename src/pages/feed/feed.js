55356d431434846c4e7686c6aba87906812c72dc
amandascam03 <amandascam03@gmail.com>
2023-07-17 17:03:38 -0300

    menu para mobile construido

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,8 +1,8 @@
 export default () => {
-    const container = document.createElement('div');
-    container.id = 'cssfeed';
+  const container = document.createElement('div');
+  container.id = 'cssfeed';
 
-    const template = `
+  const template = `
   <div>
    <nav id="menu">
      <h1>FIGHT BACK</h1>
@@ -23,8 +23,28 @@ export default () => {
        <input type="checkbox" id="check-sair">
        <label for="check">
        <img src="imagens/icon-sair.png" class="icon-sair" alt="imagem para sair "></picture></li>
-     </ul> 
+     </ul>
+     <button id="btn-menu"><img src="imagens/icon-menu.png"></button>
     </nav>
+
+    <ul class="lista-menu-mobile">
+      <li><picture> 
+      <input type="checkbox" id="check-perfil">
+      <label for="check">
+       <img src="imagens/icon-perfil.png" class="icon-perfil" alt="imagem para acessar o perfil"> <p>Perfil</p></label></picture></li>
+      <li><picture> 
+      <input type="checkbox" id="check-feed">
+      <label for="check">
+       <img src="imagens/icon-feed.png" class="icon-feed" alt="imagem para acessar o feed"><p>Feed</p></label></picture></li>
+      <li><picture>
+      <input type="checkbox" id="check-sos">
+      <label for="check">
+       <img src="imagens/icon-sos.png" class="icon-sos" alt="imagem para solicitar suporte"><p>Suporte</p></label></picture></li>
+       <li><picture>
+       <input type="checkbox" id="check-sair">
+       <label for="check">
+       <img src="imagens/icon-sair.png" class="icon-sair" alt="imagem para sair "><p>Sair</p></label></picture></li>
+     </ul>
    </div>
     <header>
      <picture id="mulher-feed"><img src="imagens/menina-feed.png"></picture>
@@ -32,7 +52,22 @@ export default () => {
      `
 
 
-    container.innerHTML = template;
+  container.innerHTML = template;
 
-    return container;
+  const botaoMenu = container.querySelector("#btn-menu");
+
+  botaoMenu.addEventListener("click", abrirMenu);
+
+  function abrirMenu() {
+    const listaMenu = container.querySelector(".lista-menu-mobile");
+
+    if (listaMenu.style.display === "none") {
+      listaMenu.style.display = "block";
+    } else {
+      listaMenu.style.display = "none";
+    }
+  }
+
+
+  return container;
 }
