720b28858b054761da56715bf79249235c53f99f
MailaFAP <maila_3@hotmail.com>
2023-07-17 18:35:52 -0300

    terminei os testes do authUser. O site está funcionando sem bugs

--- test/authUser.spec.js
+++ test/authUser.spec.js
@@ -3,31 +3,60 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signInWithPopup,
-  GoogleAuthProvider,
-  updateProfile,
-} from 'firebase/auth'; 
-import { loginEmail } from '../src/lib/authUser';
-import config from './configfirebase.js';
+  GoogleAuthProvider,  
+  auth,
+} from 'firebase/auth';
+import { loginEmail,cadastroUsuarioSenha, loginGoogle, getUserId} from '../src/lib/authUser';
+import config from '../src/lib/configfirebase';
 
-jest.mock('./configfirebase.js');
+//jest.mock('./configfirebase.js');
 jest.mock('firebase/auth');
 
 describe('login', () => {
-  it('deverá fazer o login com email e senha e depois  ', async () => {
+  it('deverá fazer o login com email e senha correta', async () => {
     const email = 'nome@algo.com';
     const senha = 'jdhjfg5246';
     await loginEmail(email, senha);// verifica a ação do usuário
-    expect(window.location.hash).toBe('#feed');// verifica o direcionamento esperado
-  });
-  it('testar para login com email e senha incorreta ', async () => {
-    const email = 'emailerrado@algo.com';
-    const senhaIncorreta = 'senhaerrada';
-    await loginEmail(email, senhaIncorreta);// verifica a ação do usuário
-    expect(window.location.hash).toBe('');// verifica o direcionamento esperado
+    expect(signInWithEmailAndPassword).toHaveBeenCalledTimes(1);
+    expect(signInWithEmailAndPassword).toHaveBeenCalledWith(auth,email, senha);// verifica o direcionamento esperado
   });
 });
-// it com os erros (no it colocar que tem que lidar com os erros)
-// testes assincronos (await e try catch)
-// usar o fail na segunda parte
-// na terceira parte usar o cath (verificar se o erro é do tipo esperado e
-// se a mensagem de erro é a esperada)
+
+describe('usuário e senha', () => {
+  it('deverá mostrar msg de erro quando email e senha estiverem incorretos', async () => {
+    const usuario = 'qualquer'
+    const email = 'nome@algo.com';
+    const senha = 'jdhjfg5246';
+    const dados = {user: usuario};
+    createUserWithEmailAndPassword.mockResolvedValue(dados);
+    await cadastroUsuarioSenha(email, senha);// verifica a ação do usuário
+    expect(createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
+    expect(createUserWithEmailAndPassword).toHaveBeenCalledWith(auth ,email, senha);// verifica o direcionamento esperado
+  });
+});
+
+describe('login com o Google', () => {
+  it('deve chamar signInWithPopup com o provedor correto', async () => {    
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth(); // Importar getAuth do Firebase e inicializar a instância do Auth
+    signInWithPopup.mockResolvedValueOnce();    
+    await loginGoogle();    
+    expect(signInWithPopup).toHaveBeenCalledWith(auth, provider);
+  });
+});
+
+describe('getUserId', () => {
+  it('should return the current user ID', () => {
+    const userId = 'user123';
+    const authMock = {
+      currentUser: {
+        uid: userId,
+      },
+    };
+    getAuth.mockReturnValue(authMock);
+    const result = getUserId();
+    expect(result).toBe(userId);
+  });
+});
+
+
