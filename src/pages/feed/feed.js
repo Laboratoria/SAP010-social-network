f5fc280152c7291759637449b81152389ffea09c
CamilaVerso <camilaverso2@gmail.com>
2023-07-17 19:53:48 -0300

    Criamos os elementos da página de feed e o evento para ler o input

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -3,6 +3,9 @@ import loguinho from '../imagens/CBDCNNCT-IMG/logologuinhamobilefeed.png';
 import perfil from '../imagens/icones/perfil.png';
 import novoPost from '../imagens/icones/novoPost.png';
 import sair from '../imagens/icones/sair.png';
+import coracao from '../imagens/icones/coracao.png';
+import editar from '../imagens/icones/editar.png';
+import excluir from '../imagens/icones/excluir.png';
 
 export default () => {
   const containerFeed = document.createElement('section');
@@ -24,33 +27,31 @@ export default () => {
   <span class="nomeUsuario"></span>
   <span class="tipoUsuario"></span>
   </div>
-  <div class="infoUserPost">
-          <div class="imgUserPost"></div>
+  <div class="containerPostVerde">
 
-          <div class="nameAndHour">
-            <strong>Douglas Pujol</strong>
-            <p>hora</p>
+          <div class="nomeTipo">
+            <strong>Usuário</strong>
+            <p>tipo</p>
           </div>
-        </div>
 
-        <p>
-         hora
-        </p>
+             <textarea name="postagem" id="text-mensagem" cols="30" rows="10"></textarea>
+
+          <button type="submit" id="btnPublicar" class="entrar centro">Publicar</button>
 
         <div class="actionBtnPost">
           <button type="button" class="filesPost like">
-            <img src="./assets/heart.svg" alt="Curtir">
-            Curtir
+            <img src=${coracao} alt="Curtir" title="Curtir">
           </button>
-          <button type="button" class="filesPost comment">
-            <img src="./assets/comment.svg" alt="Comentar">
-            Comentar
+
+          <button type="button" class="filesPost editar">
+            <img src=${editar} alt="Editar" title="Editar">
           </button>
+
           <button type="button" class="filesPost share">
-            <img src="./assets/share.svg" alt="Compartilhar">
-            Compartilhar
+            <img src=${excluir} alt="Excluir" title="Excluir">
           </button>
         </div>
+  </div>
   <footer>
   <a href="#perfil" id="iconePerfil"><img class="iconesFooter" src=${perfil} alt="icone perfil" title="Ícone Perfil"></a>
   <a href="#novoPost" id="iconeNovoPost"><img class="iconesFooter" src=${novoPost} alt="icone criar nova postagem" title="Ícone para Nova Postagem"></a>
@@ -62,10 +63,18 @@ export default () => {
 
   containerFeed.innerHTML = templateFeed;
 
+  const mensagemPost = containerFeed.querySelector('#text-mensagem');
   const btnloguinho = containerFeed.querySelector('#ir-infopage');
+  const btnPublicar = containerFeed.querySelector('#btnPublicar');
 
   btnloguinho.addEventListener('click', () => {
     window.location.hash = '#infopage';
+  });
+
+  btnPublicar.addEventListener('click', (e) => {
+    e.preventDefault();
+    const msg = mensagemPost.value;
+    console.log(msg);
   });
 
   return containerFeed;
