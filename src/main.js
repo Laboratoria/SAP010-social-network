74bf5e09cc2aa0575241e52d6fc1caae946035e6
Lila <lilaar91@gmail.com>
2023-07-17 12:12:44 -0300

    add

--- src/main.js
+++ src/main.js
@@ -3,3 +3,29 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+function renderContent(route) {
+    const contentElement = document.getElementById('content');
+    let content = '';
+
+    if (route === 'home') {
+        content = '<h1>Welcome to the Home page!</h1>';
+    } else if (route === 'about') {
+        content = '<h1>About Us</h1><p>This is the about page.</p>';
+    }else{
+        content = '<h1>Page not found</h1>';
+    }
+    contentElement.innerHTML = content;
+}
+    
+
+function handleRouteChange() {
+    const route = window.location.hash.slice(1);
+    renderContent(route);
+}
+
+// Add event listener to handle hash change
+window.addEventListener('hashchange', handleRouteChange);
+
+// Initial page load
+handleRouteChange();
