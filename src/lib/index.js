125a5d765a80ae61c2beef40e417966a8c0d1956
Lila <lilaar91@gmail.com>
2023-07-18 15:28:07 -0300

    iniciando firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,29 @@ export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
+
+export function start() {
+  const startElement = document.createElement('section');
+  startElement.setAttribute('id', 'starting');
+  startElement.setAttribute('class', 'starting');
+  startElement.innerHTML = '<h2>Welcome</h2><img src="./images/bem-vindes.gif">';
+  document.body.appendChild(startElement);
+}
+
+export function stop() {
+}
+
+export function newElement(element) {
+  const makeElement = document.createElement('section');
+  // cria uma section
+  makeElement.innerHTML = element;
+  // inclui o elemento recebido na estrutura do HTML
+  document.body.appendChild(makeElement);
+  // inclui o elemento no HTML
+}
+
+export function removeElement(element) {
+  document.body.removeChild();
+  // é possível criar um objeto bom tags HTML
+  // para que eu selecione o valor do id da tag criada pra conseguir excluir?
+}
