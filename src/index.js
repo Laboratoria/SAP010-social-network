716ed91419f1b4dd77732795f22bc3574011dd3d
Vitória Victor <vitoriavictor@Vitorias-MacBook-Pro.local>
2023-07-20 14:02:17 -0300

    autenticação funcionando

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import login from './pages/Login/login.js';
 import cadastro from './pages/Cadastro/cadastro.js';
 import feed from './pages/Feed/feed.js';
-//import { userAuthCheck } from '../src/lib/authUser.js';
-//import { userAuthCheck } from '../src/lib/authUser.js';
-
+// import { userAuthCheck } from '../src/lib/authUser.js';
+import { userAuthCheck } from './lib/authUser.js';
 const main = document.querySelector('#root');
-
-const init = () => {
+const init = async () => {
   window.addEventListener('hashchange', async () => {
     main.innerHTML = '';
     switch (window.location.hash) {
@@ -17,41 +15,42 @@ const init = () => {
       case '#cadastro':
         main.appendChild(cadastro());
         break;
-      case '#feed':
-        main.appendChild(feed());
+      // case '#feed':
+      //   main.appendChild(feed());
+      //   break;
+      case '#feed': {
+        await verificarLogin();
         break;
-/*       case '#feed':{
-          const userLogged = await userAuthCheck();
-          if (userLogged) {
-            main.appendChild(feed());
-          } else {
-            alert('Realize o login');
-            window.location.hash = '#login';
-            main.appendChild(login());
-          }
-          break;
-        } */
+      }
       default:
         main.appendChild(login());
     }
   });
 };
-
 window.addEventListener('load', () => {
   main.appendChild(login());
   init();
 });
-
-/* window.addEventListener('load', async () => {
-  const userLogged = await userAuthCheck();
-  if (userLogged) {
-    main.appendChild(feed());
-  } else {
-    window.location.hash = '#login';
-    main.appendChild(login());
-  }
-  init();
+async function verificarLogin() {
+  return userAuthCheck((userLogged) => {
+    main.innerHTML = '';
+    console.log({ userLogged });
+    if (userLogged && userLogged.email) {
+      main.appendChild(feed());
+    } else {
+      window.location.hash = '#login';
+      main.appendChild(login());
+    }
+    init();
+  });
+}
+window.addEventListener('load', async () => {
+  verificarLogin();
 });
- */
-
 // incluir validação para redirecionamento da pagina (main) video palomita
+
+
+
+
+
+
