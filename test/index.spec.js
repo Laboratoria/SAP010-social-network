94ceb7f445197fc686f0fe3ff8b748a992ed5e7d
CamilaVerso <camilaverso2@gmail.com>
2023-07-24 00:24:34 -0300

    Adicionado o teste da função deslogar

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import {
-  createUserWithEmailAndPassword, getAuth, signInWithPopup, signInWithEmailAndPassword,
+  createUserWithEmailAndPassword, getAuth, signInWithPopup, signInWithEmailAndPassword, signOut,
 } from 'firebase/auth';
-import { createUser, loginGoogle, login } from '../src/pages/serviceFirebase/firebaseAuth.js';
+import {
+  createUser, loginGoogle, login, deslogar,
+} from '../src/pages/serviceFirebase/firebaseAuth.js';
 
 jest.mock('firebase/auth');
 
@@ -52,4 +54,33 @@ describe('Login com o Google', () => {
     await loginGoogle();
     expect(signInWithPopup).toHaveBeenCalledTimes(1);
   });
+});
+
+describe('deslogar', () => {
+  it('deveria ser uma função', () => {
+    expect(typeof deslogar).toBe('function');
+  });
+
+  it('deve deslogar o usuário', async () => {
+    // Chama a função deslogar
+    await deslogar();
+
+    // Verifica se a função signOut foi chamada
+    expect(signOut).toHaveBeenCalled();
+    // Verifica se a função signOut foi chamada apenas uma vez
+    expect(signOut).toHaveBeenCalledTimes(1);
+  });
+
+  // it('Usuário não autenticado', async () => {
+  //   // Mock do getAuth para retornar um valor nulo (usuário não autenticado)
+  //   jest.spyOn(getAuth, 'getAuth').mockReturnValue(null);
+
+  //   const signOutMock = jest.fn();
+  //   signOut.mockResolvedValue(signOutMock);
+
+  //   await deslogar();
+
+  //   // Verifica se a função signOut não foi chamada, pois o usuário não está autenticado
+  //   expect(signOutMock).not.toHaveBeenCalled();
+  // });
 });
