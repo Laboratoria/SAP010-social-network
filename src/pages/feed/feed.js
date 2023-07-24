7c63d39ec5dd300d02e614f35bb0eee59cebd9a2
Diulianne Oliveira <diulicarine@gmail.com>
2023-07-24 15:33:33 -0400

    fiz alterações na função de criar post, tirei o like e adicionei o nome do usuário

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -79,6 +79,8 @@ export default () => {
     const msg = mensagemPost.value;
     console.log(msg);
     criarPost(msg);
+
+    mensagemPost.value = '';
   });
 
   btnDeslogar.addEventListener('click', async () => {
