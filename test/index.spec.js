23e1e7b78ae4c3736641df0e3d80865a6211e845
amandascam03 <amandascam03@gmail.com>
2023-07-25 17:22:54 -0300

    testando se as funçoes exportadas do firebase.js sao uma function

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,3 +6,40 @@
 //     expect(typeof myFunction).toBe('function');
 //   });
 // });
+import {
+  cadastrarUsuario,
+  atualizarNomeDoUsuario,
+  loginUsuario,
+  sairDaConta,
+  redefinirSenha,
+} from '../src/lib/firebase';
+
+describe(cadastrarUsuario, () => {
+  it('deve ser uma função', () => {
+    expect(typeof cadastrarUsuario).toBe('function');
+  });
+});
+
+describe(atualizarNomeDoUsuario, () => {
+  it('deve ser uma função', () => {
+    expect(typeof atualizarNomeDoUsuario).toBe('function');
+  });
+});
+
+describe(loginUsuario, () => {
+  it('deve ser uma função', () => {
+    expect(typeof loginUsuario).toBe('function');
+  });
+});
+
+describe(sairDaConta, () => {
+  it('deve ser uma função', () => {
+    expect(typeof sairDaConta).toBe('function');
+  });
+});
+
+describe(redefinirSenha, () => {
+  it('deve ser uma função', () => {
+    expect(typeof redefinirSenha).toBe('function');
+  });
+});
