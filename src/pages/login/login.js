961463495abdf1fcf499408dff77af507cfc6282
Daniele Maria <60490226+Danima-dev@users.noreply.github.com>
2023-08-15 11:45:21 -0300

    ajustando o template de login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,21 +4,24 @@ export default () => {
     const container = document.createElement('div');
   
     const template = `
-    <p>
-      Uma rede social segura para mulheres que curte viajar 
-      sozinha e gosta compartilhar dicas,  experiências 
-      sobre as viagens e também  fazer novas amizades.
-    </p>
-    <form>
-      <h2>Login</h2>
-      
-      <input id="email" type="text" name="email" placeholder="Seu email..."/>
-      
-      <input type="password" name="senha" placeholder="Sua senha..."/>
-      <button type="submit">ENTRAR</button>
-    </form>
-    <img src="./img/Mulheres viajando.png">
-  
+    <div class="login">
+      <div class="descricao">
+        <p>
+          Uma rede social segura para mulheres que curte viajar 
+          sozinha e gosta compartilhar dicas,  experiências 
+          sobre as viagens e também  fazer novas amizades.
+        </p>
+        
+      </div>
+      <form>
+        <h2>Login</h2>
+        
+        <input id="email" type="text" name="email" placeholder="Seu email..."/>
+        
+        <input type="password" name="senha" placeholder="Sua senha..."/>
+        <button type="submit">ENTRAR</button>
+      </form>
+    </div>
     `;
 
 
